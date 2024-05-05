9566d61084699db15685f2ad8a2bafab7e0281a6
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-05-05 08:03:24 +0530

    finish

--- 02-todo_board/src/App.js
+++ 02-todo_board/src/App.js
@@ -1,24 +1,37 @@
 import { useState } from 'react';
+import Board from "./components/Board";
 import Input from './components/Input';
 
 function App() {
 
   const [taskList, setTaskList] = useState("");
 
-  console.log(taskList);
+  
 
   return (
-    <div className='flex flex-col' items-center justify-center py-4>
-      <h1 className='text-xl font-semibold'> 02 - To Do board </h1>
+
+    <>
+      <div className='flex flex-col items-center justify-center py-4 gap-4'>
+        <h1 className='text-xl font-semibold'> 02 - To Do board </h1>
 
       <Input taskList = {taskList} setTaskList = {setTaskList} />
 
-      <div>
-        {taskList.map( (task,index) =>
-            <li key = {index}>{task} </li>
-        )}
       </div>
-    </div>
+
+      <div className='flex flex-col gap-4 sm:grid-cols-3 px-4 sm:px-8 md:px-10 lg:px-12'>
+        {taskList.map( (task,index) =>
+            <Board
+              key = {index}
+              index = {index}
+              task = {task}
+              taskList = {taskList}
+              setTaskList = {setTaskList}
+            />
+        )}
+    
+      </div>
+    </>
+    
   );
 }
 
